aea8dee75c8eccdfe94980441d1600e503119724
joyvelasquez <joy.velasquez@cfa.harvard>
2026-03-20 16:47:29 -0400

    Removing printing statement that is not need

--- xrtpy/response/temperature_response.py
+++ xrtpy/response/temperature_response.py
@@ -4,12 +4,16 @@ __all__ = [
 
 import logging
 from pathlib import Path
+import logging
+logger = logging.getLogger(__name__)
 
 import astropy.constants as const
 import numpy as np
 import scipy.io
 from astropy import units as u
 from scipy import interpolate
+
+
 
 from xrtpy.response.channel import Channel
 from xrtpy.response.effective_area import EffectiveAreaFundamental, parse_filter_input
